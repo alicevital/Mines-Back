4aea9ab1ec3f4cc3684912fd96f7c7479ccef324
Alice Vital <alice.nascimento@bsatech.io>
2025-11-25 14:43:07 -0300

    feat: adiciona rota de teste para classe de matches

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from models.matches_models import MatcheModel
 
 app = FastAPI(title="Mines Academy")
 
 print("Loaded at port 8000")
+
+@app.post("/test")
+def test_item(item: MatcheModel):
+    return item

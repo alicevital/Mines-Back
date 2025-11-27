8b0b89804b24a2ad9a658308e2edc32aaee3ffa7
Alice Emily <alice.nascimento@bsatech.io>
2025-11-27 11:08:21 -0300

    fix: corrige erro de criação de user

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,30 +1,30 @@
 from typing import List, Optional
 from bson import ObjectId
-from app.schemas.user_schema import CreateUser
+from datetime import datetime
 
 class UserRepository:
 
     def __init__(self, db):
         self.db = db
 
-    def create_user(self, user: CreateUser) -> str:
-        user_dict = user.model_dump(exclude_unset=True)
+    def create_user(self, user_dict: dict) -> dict:
+        # insere create_at automaticamente
+        user_dict["create_at"] = datetime.utcnow()
+
         result = self.db.users.insert_one(user_dict)
 
-        if not result.inserted_id:
-            raise Exception("Falha ao inserir usuário no DB")
-        
+        user_dict["_id"] = result.inserted_id
         return user_dict
     
-
+    def get_user_by_name(self, name: str):
+        return self.db.users.find_one({"name": name})
+    
     def get_user_by_id(self, user_id: str) -> Optional[dict]:
         return self.db.users.find_one({"_id": ObjectId(user_id)})
-    
 
     def get_all_users(self) -> List[dict]:
         return list(self.db.users.find())
     
-
     def delete_user(self, user_id: str) -> bool:
         result = self.db.users.delete_one({"_id": ObjectId(user_id)})
 

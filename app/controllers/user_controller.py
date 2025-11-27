52e42e95a13711c922ea2edfd4ddc03d95f297be
Alice Emily <alice.nascimento@bsatech.io>
2025-11-27 11:07:54 -0300

    fix: corrige erro de conflito do banco

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,27 +1,24 @@
-from app.schemas.user_schema import CreateUser, UserSchemaBody
+from app.schemas.user_schema import CreateUser
 from fastapi import APIRouter, Depends
 from app.services.user_service import UserService
 from app.repositories.user_repository import UserRepository
 from typing import List
+from app.database.db import get_database
 
 UserRouter = APIRouter(tags=['Rotas de usuários'])
 
 def get_db():
-    db = get_db()
-    try:
-        yield db
-    finally:
-        pass
+    return get_database()
 
 def get_user_service(db=Depends(get_db)) -> UserService:
     repository = UserRepository(db)
     return UserService(repository)
 
-@UserRouter.post("/users/register", response_model=UserSchemaBody)
-def create_user(user: UserSchemaBody, service: UserService = Depends(get_user_service)):
+@UserRouter.post("/users/register", response_model=CreateUser)
+def create_user(user: CreateUser, service: UserService = Depends(get_user_service)):
     return service.create_user(user)
 
-@UserRouter.get("/users/", response_model=List[UserSchemaBody])
+@UserRouter.get("/users/", response_model=List[CreateUser])
 def list_all_users(service: UserService = Depends(get_user_service)):
     return service.get_all_users()
 
